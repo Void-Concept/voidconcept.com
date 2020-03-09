88fdd0df97759aa6ff804141a24baebff8f7c596
VoidConcept <voidconception@gmail.com>
2020-03-08 20:02:13 -0700

    Redirect to the only implemented page for now

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { Router, Route } from 'react-router';
+import { Router, Route, Redirect, Switch } from 'react-router';
 import { createBrowserHistory } from 'history';
 import { SpellbookComponent } from './dnd/spellbook/SpellbookComponent';
 
 function App() {
     return (
         <Router history={createBrowserHistory()}>
-            <Route path="/dnd/spellbook">
-                <SpellbookComponent />
-            </Route>
+            <Switch>
+                <Route path="/dnd/spellbook">
+                    <SpellbookComponent />
+                </Route>
+                <Route>
+                    <Redirect to="/dnd/spellbook" />
+                </Route>
+            </Switch>
         </Router>
     );
 }
