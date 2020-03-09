ad289ffdfb553cfafa31214d40e725325534b162
VoidConcept <voidconception@gmail.com>
2020-03-08 19:00:17 -0700

    Deploy script deletes old artifacts in s3

--- frontend/scripts/deploy.sh
+++ frontend/scripts/deploy.sh
@@ -1,3 +1,3 @@
 yarn build --cwd .. &&
 S3Name=$(aws cloudformation describe-stacks --stack-name FrontendStack --query 'Stacks[0].Outputs[?OutputKey == `FrontendBucketUri`].OutputValue' --output text) &&
-aws s3 sync ../build ${S3Name}
+aws s3 sync ../build ${S3Name} --delete
