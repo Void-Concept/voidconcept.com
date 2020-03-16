83b0be510e4ea61fa1989c1796271ee8d7fea074
VoidConcept <voidconception@gmail.com>
2020-03-15 20:17:37 -0700

    Read/write calendar

--- globalStorageLambda/src/index.ts
+++ globalStorageLambda/src/index.ts
@@ -1,16 +1,35 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+import { DynamoHelper } from "./dynamoHelper";
+import * as DynamoDB from 'aws-sdk/clients/dynamodb';
 
 export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-    return await dependencyInjectedHandler(event);
+    const dynamoDbTableName = process.env.tableName as string;
+    const dynamoDb = new DynamoDB();
+    const dynamoHelper = new DynamoHelper(dynamoDb, dynamoDbTableName);
+
+    return await dependencyInjectedHandler(event, dynamoHelper);
 }
 
 export const dependencyInjectedHandler = async (
-    event: APIGatewayProxyEvent
+    event: APIGatewayProxyEvent,
+    dynamoHelper: DynamoHelper
 ): Promise<APIGatewayProxyResult> => {
-    console.log(event);
-    return {
-        statusCode: 200,
-        body: JSON.stringify({})
+    if (event.httpMethod === "GET") {
+        const calendar = await dynamoHelper.getDndCalendar();
+        return {
+            statusCode: 200,
+            body: JSON.stringify(calendar)
+        };
+    } else if (event.httpMethod === "POST") {
+        return {
+            statusCode: 501,
+            body: "Not implemented"
+        }
+    } else {
+        return {
+            statusCode: 404,
+            body: "Operation not found"
+        }
     }
 }
 
