dd50d81e3faa0cd8ce61f5ffdf4786c0cd3f4113
VoidConcept <voidconception@gmail.com>
2020-08-08 15:45:23 -0700

    Added Scrying
    (cherry picked from commit 19c0382693509e43cf4a15e96968357f9d888da5)

--- frontend/src/dnd/spellbook/Spellbook.ts
+++ frontend/src/dnd/spellbook/Spellbook.ts
@@ -492,5 +492,29 @@ export const spells: Spell[] = [
             "Interposing Hand. The hand interposes itself between you and a creature you choose until you give the hand a different command. The hand moves to stay between you and the target, providing you with half cover against the target.The target can't move through the hand's space if its Strength score is less than or equal to the hand's Strength score. If its Strength score is higher than the hand's Strength score, the target can move toward you through the hand's space, but that space is difficult terrain for the target."
         ],
         higherLevel: "When you cast this spell using a spell slot of 6th level or higher, the damage from the clenched fist option increases by 2d8 and the damage from the grasping hand increases by 2d6 for each slot level above 5th."
+    }, {
+        name: "Scrying",
+        level: "5th",
+        school: "Divination",
+        castTime: "10 minutes",
+        range: "Self",
+        components: "V, S, M",
+        materials: "A focus worth at least 1,000 gp, such as a crystal ball, a silver mirror, or a font filled with holy water",
+        duration: "10 minutes",
+        concentration: true,
+        description: [
+            "You can see and hear a particular creature you choose that is on the same plane of existence as you. The target must make a Wisdom saving throw, which is modified by how well you know the target and the sort of physical connection you have to it. If a target knows you’re casting this spell, it can fail the saving throw voluntarily if it wants to be observed.",
+            "Knowledge Save Modifier",
+            "Secondhand(you have heard of the target) + 5",
+            "Firsthand(you have met the target) + 0",
+            "Familiar(you know the target well) −5",
+            "Connection Save Modifier",
+            "Likeness or picture −2",
+            "Possession or garment −4",
+            "Body part, lock of hair, bit of nail, or the like −10",
+            "On a successful save, the target isn’t affected, and you can’t use this spell against it again for 24 hours.",
+            "On a failed save, the spell creates an invisible sensor within 10 feet of the target.You can see and hear through the sensor as if you were there.The sensor moves with the target, remaining within 10 feet of it for the duration.A creature that can see invisible objects sees the sensor as a luminous orb about the size of your fist.",
+            "Instead of targeting a creature, you can choose a location you have seen before as the target of this spell.When you do, the sensor appears at that location and doesn’t move.",
+        ]
     },
 ];
