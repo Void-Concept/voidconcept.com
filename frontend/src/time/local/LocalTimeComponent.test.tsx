d6b9a1fec08b8f4fa189e750921ee06d7d43a8bd
VoidConcept <voidconception@gmail.com>
2020-09-04 15:11:17 -0700

    Fixed LocalTimeComponent tests

--- frontend/src/time/local/LocalTimeComponent.test.tsx
+++ frontend/src/time/local/LocalTimeComponent.test.tsx
@@ -2,20 +2,32 @@ import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
 import { LocalTimeComponent, LocalTimeComponentParams } from './LocalTimeComponent';
 import moment from 'moment-timezone';
-import { createMemoryHistory } from 'history';
-import { match as Match } from 'react-router';
+import { createMemoryHistory, History } from 'history';
+import { match as Match, Router, Route } from 'react-router';
 
 describe("LocalTimezoneComponent", () => {
+    const renderComponent = (history: History) => {
+        const wrapper = render(
+            <Router history={history}>
+                <Route path="/time/local/:epochTime">
+                    <LocalTimeComponent />
+                </Route>
+                <Route path="/time/local/" exact>
+                    <LocalTimeComponent />
+                </Route>
+            </Router>
+        );
+        return wrapper;
+    }
+
     it("should display current time in local timezone", async () => {
-        const match = {
-            params: {
-                epochTime: "1583804488000"
-            }
-        } as Match<LocalTimeComponentParams>;
+        const history = createMemoryHistory({
+            initialEntries: [`/time/local/1583804488000`]
+        });
 
-        const wrapper = render(<LocalTimeComponent match={match} history={createMemoryHistory()} />);
+        const wrapper = renderComponent(history);
 
-        expect(await wrapper.findByDisplayValue("2020-03-09 06:03:00 PM")).toBeDefined();
+        expect(await wrapper.findByDisplayValue("2020-03-09 06:41:00 PM")).toBeDefined();
     });
 
     it("should move path on change", async () => {
@@ -30,11 +42,11 @@ describe("LocalTimezoneComponent", () => {
             }
         } as Match<LocalTimeComponentParams>;
 
-        const wrapper = render(<LocalTimeComponent match={match} history={history} />);
+        const wrapper = renderComponent(history);
 
         const expectedEpochTime = "1583804512000";
 
-        const input = await wrapper.findByDisplayValue("2020-03-09 06:03:00 PM") as HTMLInputElement;
+        const input = await wrapper.findByDisplayValue("2020-03-09 06:41:00 PM") as HTMLInputElement;
         fireEvent.change(input, {
             target: {
                 value: moment(expectedEpochTime, 'x').toDate()
@@ -48,23 +60,15 @@ describe("LocalTimezoneComponent", () => {
         const history = createMemoryHistory({
             initialEntries: [`/time/local`]
         });
-        const match = {
-            params: {}
-        } as Match<LocalTimeComponentParams>;
 
-        render(<LocalTimeComponent match={match} history={history} />);
+        renderComponent(history);
     });
 
     it("should still display with invalid time", async () => {
         const history = createMemoryHistory({
             initialEntries: [`/time/local/notanumber`]
         });
-        const match = {
-            params: {
-                epochTime: "notanumber"
-            }
-        } as Match<LocalTimeComponentParams>;
 
-        render(<LocalTimeComponent match={match} history={history} />);
+        renderComponent(history);
     });
 });
