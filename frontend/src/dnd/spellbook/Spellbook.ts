81bbc3e3ac84dec63182db493222736147ba1ec2
VoidConcept <voidconception@gmail.com>
2020-09-26 23:24:51 -0700

    Added Teleportation Circle to spellbook

--- frontend/src/dnd/spellbook/Spellbook.ts
+++ frontend/src/dnd/spellbook/Spellbook.ts
@@ -516,5 +516,20 @@ export const spells: Spell[] = [
             "On a failed save, the spell creates an invisible sensor within 10 feet of the target.You can see and hear through the sensor as if you were there.The sensor moves with the target, remaining within 10 feet of it for the duration.A creature that can see invisible objects sees the sensor as a luminous orb about the size of your fist.",
             "Instead of targeting a creature, you can choose a location you have seen before as the target of this spell.When you do, the sensor appears at that location and doesn’t move.",
         ]
+    }, {
+        name: "Teleportation Circle",
+        level: "5th",
+        school: "Conjuration",
+        castTime: "1 minute",
+        range: "10 ft",
+        areaOfEffect: "10 ft",
+        components: "V, M",
+        materials: "rare chalks and inks infused with precious gems worth 50 gp, which the spell consumes",
+        duration: "1 round",
+        description: [
+            "As you cast the spell, you draw a 10-foot-diameter circle on the ground inscribed with sigils that link your location to a permanent teleportation circle of your choice whose sigil sequence you know and that is on the same plane of existence as you. A shimmering portal opens within the circle you drew and remains open until the end of your next turn. Any creature that enters the portal instantly appears within 5 feet of the destination circle or in the nearest unoccupied space if that space is occupied.",
+            "Many major temples, guilds, and other important places have permanent teleportation circles inscribed somewhere within their confines. Each such circle includes a unique sigil sequence--a string of magical runes arranged in a particular pattern. When you first gain the ability to cast this spell, you learn the sigil sequences for two destinations on the Material Plane, determined by the GM. You can learn additional sigil sequences during your adventures. You can commit a new sigil sequence to memory after studying it for 1 minute.",
+            "You can create a permanent teleportation circle by casting this spell in the same location every day for one year. You need not use the circle to teleport when you cast the spell in this way."
+        ]
     },
 ];
