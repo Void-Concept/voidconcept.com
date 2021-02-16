1816cd2f1c6db4f1e300c4637866d8260de43a1b
VoidConcept <voidconception@gmail.com>
2021-02-15 19:54:06 -0800

    Refactor quest component to make simpler

--- frontend/src/runescape/quests/QuestsComponent.tsx
+++ frontend/src/runescape/quests/QuestsComponent.tsx
@@ -24,7 +24,7 @@ type QuestList = {
 
 type QuestCell = {
     user: string
-    questName: string
+    title: string
     status: string
     difficulty: number
     members: boolean
@@ -33,50 +33,41 @@ type QuestCell = {
 }
 
 type QuestRow = QuestCell[]
+type QuestTable = QuestRow[]
 
-export const QuestsComponent = () => {
-    const [questLists, setQuestLists] = useState<QuestList[]>([]);
+type QuestTableRowProps = {
+    questRow: QuestRow
+}
+const QuestTableRow = ({ questRow }: QuestTableRowProps) => {
+    const questName = questRow[0]?.title
+    return (
+        <Row key={questName}>
+            <Cell>
+                {questName}
+            </Cell>
+            {questRow.map(questCell => {
+                const className = getStatusClass(questCell.status)
+                return (
+                    <Cell className={className}>
+                        {questCell.user} {questCell.status} {`${questCell.userEligible}`}
+                    </Cell>
+                )
+            })}
+        </Row>
+    )
+}
 
-    const [users, setUsers] = useState<string[]>(['void_cosmos', 'sapphisms', 'issybelle', 'amibelle', 'aimasu', 'madidle'])
-    useEffect(() => {
-        const doFetch = async () => {
-            const questPromises = users.map(async user => {
-                const response = await fetch(`https://runescape.voidconcept.com/runemetrics/quests?user=${user}`, {
-                    mode: "cors"
-                })
-                const responseJson: QuestResponse = await response.json()
-                return {
-                    user,
-                    quests: responseJson.quests
-                }
-            })
-            const quests = await Promise.all(questPromises)
-            setQuestLists(quests)
-        }
-        doFetch().catch(console.error)
-    }, [users])
-
-    const questTable = useMemo(() => {
-        const flattenedQuests = questLists.flatMap(questList => {
-            return questList.quests.map(quest => {
-                return {
-                    user: questList.user,
-                    questName: quest.title,
-                    status: quest.status,
-                    difficulty: quest.difficulty,
-                    members: quest.members,
-                    userEligible: quest.userEligible,
-                    questPoints: quest.questPoints
-                }
-            })
-        })
-        return R.groupBy((quest: QuestCell) => quest.questName)(flattenedQuests)
-    }, [questLists])
+type QuestTableProps = {
+    users: string[]
+    questTable: QuestTable
+}
+const QuestTableComponent = ({ users, questTable }: QuestTableProps) => {
+    const rows = questTable.map((questRow, index) => <QuestTableRow questRow={questRow} key={index} />)
 
     return (
         <Table>
             <Row>
-                <Cell></Cell>
+                <Cell />
                 {users.map((user, index) => {
                     return (
                         <Cell key={index}>
@@ -85,25 +76,57 @@ export const QuestsComponent = () => {
                     )
                 })}
             </Row>
-            {Object.values(R.mapObjIndexed((questRows: QuestCell[], questName: string) => {
-                return (
-                    <Row key={questName}>
-                        <Cell>
-                            {questName}
-                        </Cell>
-                        {questRows.map(questRow => {
-                            const className = getStatusClass(questRow.status)
-                            return (
-                                <Cell className={className}>
-                                    {questRow.user} {questRow.status} {`${questRow.userEligible}`}
-                                </Cell>
-                            )
-                        })}
-                    </Row>
-                )
-            })(questTable))}
-        </Table>
+            {rows}
+        </Table >
+    )
+}
+
+export const QuestsComponent = () => {
+    const [users, setUsers] = useState<string[]>(['void_cosmos', 'sapphisms', 'issybelle', 'amibelle', 'aimasu', 'madidle'])
+    const [questTable, setQuestTable] = useState<QuestRow[]>([]);
+
+    useEffect(() => {
+        const doFetch = async () => {
+            const questPromises = users.map(fetchUserData)
+            const questsPerUser = await Promise.all(questPromises)
+            const questsByQuest = groupByQuest(questsPerUser)
+
+            setQuestTable(questsByQuest)
+        }
+        doFetch().catch(console.error)
+    }, [users])
+
+    return (
+        <QuestTableComponent users={users} questTable={questTable} />
     );
+}
+
+const fetchUserData = async (user: string): Promise<QuestCell[]> => {
+    const response = await fetch(`https://runescape.voidconcept.com/runemetrics/quests?user=${user}`, {
+        mode: "cors"
+    })
+    const responseJson: QuestResponse = await response.json()
+    return responseJson.quests.map(quest => ({
+        user,
+        ...quest
+    }))
+}
+
+type QuestGroupReduceResults = {
+    [questName: string]: QuestCell[]
+}
+const groupByQuest = (questsPerUser: QuestCell[][]): QuestRow[] => {
+    const groupedQuests = questsPerUser.flat().reduce<QuestGroupReduceResults>((previous, current) => {
+        const key = current.title
+        const oldValue = previous[key] || []
+        return {
+            ...previous,
+            [key]: [...oldValue, current]
+        }
+    }, {})
+
+
+    return Object.values(groupedQuests)
 }
 
 const getStatusClass = (status: string) => {
