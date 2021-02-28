41e76d4b6d15a9ec41e983eeb9e2cf27b202ff5c
VoidConcept <voidconception@gmail.com>
2021-02-27 22:38:55 -0800

    Make Water Breathing ritual

--- frontend/src/dnd/spellbook/Spellbook.ts
+++ frontend/src/dnd/spellbook/Spellbook.ts
@@ -442,6 +442,7 @@ export const spells: Spell[] = [
         components: "V, S, M",
         materials: "A short reed or piece of straw",
         duration: "24 hours",
+        ritual: true,
         description: [
             "This spell grants up to ten willing creatures you can see within range the ability to breathe underwater until the spell ends. Affected creatures also retain their normal mode of respiration.",
         ],
