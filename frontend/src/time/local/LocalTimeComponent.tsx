09f2a7f7b3cccf2e310c2c987bb9899eb01e02c0
VoidConcept <voidconception@gmail.com>
2022-03-15 19:51:18 -0700

    Add discord helpers to time picker

--- frontend/src/time/local/LocalTimeComponent.tsx
+++ frontend/src/time/local/LocalTimeComponent.tsx
@@ -19,6 +19,17 @@ export const LocalTimeComponent = () => {
     const epochTime = getEpochTime(match.params.epochTime)
     const time = moment(epochTime)
     const abbrZone = moment.tz.zone(moment.tz.guess())?.abbr(epochTime)
+    const zoneOffset = time.utcOffset() / 60
+    
+    const discordHelperTimes = [
+        [`<t:${epochTime}:d>`, time.format("MM/DD/YYYY")],
+        [`<t:${epochTime}:f>`, time.format("MMMM DD, YYYY H:mm A")],
+        [`<t:${epochTime}:t>`, time.format("H:mm A")],
+        [`<t:${epochTime}:D>`, time.format("MMMM DD, YYYY")],
+        [`<t:${epochTime}:F>`, time.format("dddd, MMMM DD, YYYY H:mm A")],
+        [`<t:${epochTime}:T>`, time.format("H:mm:ss A")],
+        [`<t:${epochTime}:R>`, time.fromNow()],
+    ]
 
     return (
         <div className="time-local-container">
@@ -35,7 +46,15 @@ export const LocalTimeComponent = () => {
                     dateFormat="yyyy-MM-dd hh:mm:SS a"
                 />
                 <span className="time-local-zone">{abbrZone}</span>
+                {/* <input type="number" value={zoneOffset}/> */}
             </div>
+            <ul>
+                {discordHelperTimes.map((time, index) => 
+                    <li className="time-local-zone" key={index}>
+                        {time[0]} - {time[1]}
+                    </li>
+                )}
+            </ul>
         </div>
     );
 }
