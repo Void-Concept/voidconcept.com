8e3918816e4291be54d94691672a98f136e10278
VoidConcept <voidconception@gmail.com>
2022-07-09 20:21:46 -0700

    Add Fly

--- frontend/src/dnd/spellbook/Spellbook.ts
+++ frontend/src/dnd/spellbook/Spellbook.ts
@@ -520,6 +520,19 @@ export const spells: Spell[] = [
             "This spell grants up to ten willing creatures you can see within range the ability to breathe underwater until the spell ends. Affected creatures also retain their normal mode of respiration.",
         ],
     }, {
+        name: "Fly",
+        level: "3rd",
+        school: "Transmutation",
+        castTime: "1 action",
+        range: "touch",
+        components: "V, S, M",
+        materials: "A wing feather from any bird",
+        duration: "10 Minutes",
+        description: [
+            "You touch a willing creature. The target gains a flying speed of 60 feet for the duration. When the spell ends, the target falls if it is still aloft, unless it can stop the fall.",
+        ],
+        higherLevel: "When you cast this spell using a spell slot of 4th level or higher, you can target one additional creature for each slot level above 3rd."
+    }, {
         name: "Polymorph",
         level: "4th",
         school: "Transmutation",
