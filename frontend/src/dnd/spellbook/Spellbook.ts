c7588dcbe1c6c735caa4982f1deac15f5af8b159
VoidConcept <voidconception@gmail.com>
2023-02-01 19:02:09 -0800

    Add Dominate Monster

--- frontend/src/dnd/spellbook/Spellbook.ts
+++ frontend/src/dnd/spellbook/Spellbook.ts
@@ -934,5 +934,21 @@ export const spells: Spell[] = [
             "Creatures and Objects. A creature or object summoned or created by magic temporarily winks out of existence in the sphere. Such a creature instantly reappears once the space the creature occupied is no longer within the sphere.",
             "Dispel Magic. Spells and magical effects such as dispel magic have no effect on the sphere. Likewise, the spheres created by different antimagic field spells don't nullify each other.",
         ],
+    }, {
+        name: "Dominate Monster",
+        level: "8th",
+        school: "Enchantment",
+        castTime: "1 action",
+        range: "60ft",
+        components: "V, S",
+        duration: "1 hour",
+        concentration: true,
+        description: [
+            "You attempt to beguile a creature that you can see within range. It must succeed on a Wisdom saving throw or be charmed by you for the duration. If you or creatures that are friendly to you are fighting it, it has advantage on the saving throw.",
+            `While the creature is charmed, you have a telepathic link with it as long as the two of you are on the same plane of existence. You can use this telepathic link to issue commands to the creature while you are conscious (no action required), which it does its best to obey. You can specify a simple and general course of action, such as "Attack that creature," "Run over there," or "Fetch that object." If the creature completes the order and doesn't receive further direction from you, it defends and preserves itself to the best of its ability.`,
+            "You can use your action to take total and precise control of the target. Until the end of your next turn, the creature takes only the actions you choose, and doesn't do anything that you don't allow it to do. During this time, you can also cause the creature to use a reaction, but this requires you to use your own reaction as well.",
+            "Each time the target takes damage, it makes a new Wisdom saving throw against the spell. If the saving throw succeeds, the spell ends.",
+        ],
+        higherLevel: "When you cast this spell with a 9th-level spell slot, the duration is concentration, up to 8 hours.",
     },
 ];
