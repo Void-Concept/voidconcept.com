705fe3236a909f41ab4fb8bb8c8ab038da7e54b7
VoidConcept <voidconception@gmail.com>
2024-06-15 18:54:44 -0700

    Set Misty Step and Silvery Barbs to always be prepared

--- frontend/src/dnd/spellbook/Spellbook.ts
+++ frontend/src/dnd/spellbook/Spellbook.ts
@@ -318,6 +318,7 @@ export const spells: Spell[] = [
         range: "60 ft",
         components: "V",
         duration: "Instantaneous",
+        alwaysPrepared: true,
         description: [
             "You magically distract the triggering creature and turn its momentary uncertainty into encouragement for another creature. The triggering creature must reroll the d20 and use the lower roll.",
             "You can then choose a different creature you can see within range (you can choose yourself). The chosen creature has advantage on the next attack roll, ability check, or saving throw it makes within 1 minute. A creature can be empowered by only one use of this spell at a time.",
@@ -344,6 +345,7 @@ export const spells: Spell[] = [
         range: "Self",
         components: "V",
         duration: "Instant",
+        alwaysPrepared: true,
         description: [
             "Briefly surrounded by silvery mist, you teleport up to 30 feet to an unoccupied space that you can see."
         ],
